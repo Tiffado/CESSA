f2190085a3b9e45ff993371aeb550a44c4ced14c
Lionel Bécuwe <lionel.becuwe@gmail.com>
2012-10-03 14:56:28 +0200

    Hello World

---
+++ Restlet/src/FirstServerResource.java
@@ -0,0 +1,40 @@
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+
+public class FirstServerResource extends ServerResource {
+
+	   public static void main(String[] args) throws Exception {  
+		      // Create the HTTP server and listen on port 8182  
+		      new Server(Protocol.HTTP, 8182, FirstServerResource.class).start();  
+		   
+	   //*
+		   // Outputting the content of a Web page  
+		      new ClientResource("http://localhost:8182").get().write(System.out);  
+	   //*/
+	   
+		   /*
+		      // Create the client resource  
+		      ClientResource resource = new ClientResource("http://www.restlet.org");  
+		       
+		      // Customize the referrer property  
+		      resource.setReferrerRef("http://www.mysite.org");  
+
+		      // Write the response entity on the console
+		      resource.get().write(System.out);  
+	   //*/
+	   
+	   
+	   }
+
+	   @Get  
+	   public String toString() {  
+	      return "hello, world";
+	   }
+
+		
+	
+}
