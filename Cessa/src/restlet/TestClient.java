41810566e69e324e83e74d6568f553908278d7df
Paul Blouët <paul@paulblouet.fr>
2012-10-24 22:50:39 +0200

    Explicitely GPL

--- Cessa/src/restlet/TestClient.java
+++ Cessa/src/restlet/TestClient.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2012 Lionel Bécuwe, Paul Blouët, Fabien Lamarque, Frédéric Lucas
+ *
+ * This file is part of aca-middleware.
+ *
+ * aca-middleware is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aca-middleware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aca-middleware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package restlet;
 
 import org.restlet.resource.ClientResource;
